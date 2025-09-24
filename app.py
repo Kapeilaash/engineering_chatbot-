a789d760e21760880a23ba97601c549b2bf70999
Kapeilaash Koneswaran <165775296+Kapeilaash@users.noreply.github.com>
2025-09-24 12:17:35 +0530

    Change index name from 'medical-chatbot' to 'Engineering-chatbot'

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 embeddings = download_hugging_face_embeddings()
 
-index_name = "medical-chatbot" 
+index_name = "Enginnering-chatbot" 
 # Embed each chunk and upsert the embeddings into your Pinecone index.
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
